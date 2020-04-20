c50336925f2f30f6f63e2ee970df9b2b03f5f853
Khalid Al-Awady <pipes_and_bottles@protonmail.com>
2020-04-20 15:09:44 -0400

    separating classes into proper files

---
+++ data_class.py
@@ -0,0 +1,35 @@
+#creates the dbObject and then the rest of the relational database structure based on site location
+#can be joined using the IDs, authors or other values
+class dbObject:
+    def __init__(self,db):
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        cursor.execute("select distinct site from doe_base;")
+        uniqueSites = cursor.fetchall()
+        for site in uniqueSites:
+            siteName = site[0]
+            siteName = siteName.split(" ")
+            siteName = "_".join(siteName)
+            sql = "create table if not exists " +  siteName + " as "
+            sql += """SELECT ID, date, Site, author_1, author_2, ANALYSIS, 
+                    SOFTWARE, EMBEDDED, DATABASE, SPREADSHEET, FIRMWARE,
+                    CALCULATION, PROGRAMMING, TEST FROM DOE_BASE 
+                    where site = ?;"""
+            cursor.execute(sql, site)
+        cursor.close()
+
+    
+    def splitDataBySite(self):
+        #work in progress
+        #change to lambda
+        print("new object")
+    
+    def splitDataByAuthor(self):
+        #work in progress
+        #change to lambda
+        print("new object")
+    
+    def splitDataByTerm(self):
+        #work in progress
+        #change to lambda
+        print("new object")
